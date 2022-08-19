78bc09f291e464655f2c8d59e687aabc9a8db928
Nikolos-S <sarkozzy@mail.ru>
2022-08-19 11:30:16 +0700

    delete unnecessary files

--- src/view/renderLoading.js
+++
@@ -1,9 +0,0 @@
-export default (message, elements) => {
-  // const parent = elements.input.closest('.text-white');
-  // const pEl = document.createElement('p');
-  // pEl.classList.add('feedback', 'm-0', 'position-absolute', 'small');
-  // pEl.classList.add('text-info');
-  // pEl.textContent = message;
-  // parent.replaceChild(pEl, parent.querySelector('.feedback'));
-  console.log(message);
-};
