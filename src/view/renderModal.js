bc381450f68a6651e349b35208dafa0fad00edff
Nikolos-S <sarkozzy@mail.ru>
2022-08-22 12:51:49 +0700

    refact modal

--- src/view/renderModal.js
+++ src/view/renderModal.js
@@ -1,8 +1,10 @@
 export default (post, elements) => {
   const title = elements.modalWindow.querySelector('h5');
   const description = elements.modalWindow.querySelector('.modal-body');
-  title.textContent = post.title;
-  description.textContent = post.description;
+  const activePost = post[0];
+  title.textContent = activePost.title;
+  description.textContent = activePost.description;
   const aEl = elements.modalWindow.querySelector('a');
-  aEl.setAttribute('href', post.link);
+  aEl.setAttribute('href', activePost.link);
+  console.log(post);
 };
