502e58586c6d2a220668ceaddcba71c7b29c8fea
Nikolos-S <sarkozzy@mail.ru>
2022-09-07 13:16:29 +0700

    buildPosts/feeds

--- src/view/buildFeed.js
+++ src/view/buildFeed.js
@@ -1,17 +1,32 @@
 const buildFeed = (feeds, i18nInstance, elements) => {
   const container = elements.containerFeeds;
-  container.innerHTML = `<div><div><h2>${i18nInstance.t('feeds')}</h2></div><ul></ul></div>`;
-  const listFeed = feeds.reduce((acc, feed) => `${acc}<li><h3>${feed.titleFeed}</h3><p>${feed.descriptionFeed}</p></li>`, '');
-  container.querySelector('ul').innerHTML = listFeed;
-  const divBorder = container.querySelector('div');
-  divBorder.classList.add('card', 'border-0');
-  const divBody = divBorder.querySelector('div');
-  divBody.classList.add('card-body');
-  container.querySelector('h2').classList.add('card-title', 'h4');
-  container.querySelector('ul').classList.add('list-group', 'border-0', 'rounded-0');
-  container.querySelectorAll('li').forEach((liEl) => liEl.classList.add('list-group-item', 'border-0', 'border-end-0'));
-  container.querySelectorAll('h3').forEach((hEl) => hEl.classList.add('h6', 'm-0'));
-  container.querySelectorAll('p').forEach((pEl) => pEl.classList.add('m-0', 'small', 'text-black-50'));
+  container.innerHTML = '';
+  const feedContainer = document.createElement('div');
+  const littleDiv = document.createElement('div');
+  const titleFeed = document.createElement('h2');
+  const ulList = document.createElement('ul');
+  feedContainer.classList.add('card', 'border-0');
+  littleDiv.classList.add('card-body');
+  ulList.classList.add('list-group', 'border-0', 'rounded-0');
+  titleFeed.classList.add('card-title', 'h4');
+  titleFeed.textContent = `${i18nInstance.t('feeds')}`;
+  littleDiv.append(titleFeed);
+  feedContainer.append(littleDiv);
+  feedContainer.append(ulList);
+  container.append(feedContainer);
+  feeds.forEach((feed) => {
+    const list = document.createElement('li');
+    const titleList = document.createElement('h3');
+    const description = document.createElement('p');
+    list.classList.add('list-group-item', 'border-0', 'border-end-0');
+    titleList.classList.add('h6', 'm-0');
+    description.classList.add('m-0', 'small', 'text-black-50');
+    titleList.textContent = `${feed.titleFeed}`;
+    description.textContent = `${feed.descriptionFeed}`;
+    list.append(titleList);
+    list.append(description);
+    ulList.append(list);
+  });
 };
 
 export default buildFeed;
--- src/view/buildPosts.js
+++ src/view/buildPosts.js
@@ -1,22 +1,45 @@
-import updatePosts from './update.js';
-
 const getNewPosts = (newPost, prevPosts) => newPost
   .filter((element) => prevPosts
     .findIndex((el) => el.title === element.title) === -1);
 
 const buildPosts = (newPosts, prevPosts, i18nInstance, elements) => {
-  if (!elements.containerPosts.querySelector('ul')) {
-    elements.containerPosts.innerHTML = `<div><div><h2>${i18nInstance.t('posts')}</h2></div><ul></ul></div>`;
-    const divBorder = elements.containerPosts.querySelector('div');
-    divBorder.classList.add('card', 'border-0');
-    const divBody = divBorder.querySelector('div');
-    divBody.classList.add('card-body');
-    elements.containerPosts.querySelector('h2').classList.add('card-title', 'h4');
-    const ulEl = elements.containerPosts.querySelector('ul');
-    ulEl.classList.add('list-group', 'border-0', 'rounded-0');
+  const container = elements.containerPosts;
+  if (!container.querySelector('ul')) {
+    const postContainer = document.createElement('div');
+    const littleDiv = document.createElement('div');
+    const titlePost = document.createElement('h2');
+    const ulList = document.createElement('ul');
+    postContainer.classList.add('card', 'border-0');
+    littleDiv.classList.add('card-body');
+    titlePost.classList.add('card-title', 'h4');
+    ulList.classList.add('list-group', 'border-0', 'rounded-0');
+    titlePost.textContent = `${i18nInstance.t('posts')}`;
+    littleDiv.append(titlePost);
+    postContainer.append(littleDiv);
+    postContainer.append(ulList);
+    container.append(postContainer);
   }
+  const ulEl = container.querySelector('ul');
   const currentPost = getNewPosts(newPosts, prevPosts);
-  updatePosts(currentPost, i18nInstance);
+  currentPost.forEach((post) => {
+    const liEl = document.createElement('li');
+    const aEl = document.createElement('a');
+    const buttonEl = document.createElement('button');
+    aEl.classList.add('fw-bold');
+    buttonEl.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+    liEl.classList.add('list-group-item', 'border-0', 'border-end-0', 'd-flex', 'justify-content-between', 'align-items-start');
+    aEl.setAttribute('href', post.link);
+    aEl.setAttribute('target', '_blank');
+    aEl.setAttribute('data-id', post.id);
+    buttonEl.dataset.bsToggle = 'modal';
+    buttonEl.dataset.bsTarget = '#modal';
+    buttonEl.dataset.id = post.id;
+    aEl.textContent = post.title;
+    buttonEl.textContent = i18nInstance.t('vewing');
+    liEl.append(aEl);
+    liEl.append(buttonEl);
+    ulEl.append(liEl);
+  });
 };
 
 export default buildPosts;
